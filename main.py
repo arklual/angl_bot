08441ca0a2735f212c1797e0553aef1e33c4c2f5
arklual <arklual1@gmail.com>
2023-07-19 16:26:15 +0300

    update

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ async def request_to_gpt(user_id, text):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=data,
-        max_tokens=100+(len(text)//4)
+        max_tokens=80+(len(text)//4)
     )
     response = completion['choices'][0]['message']['content']
     await append_messages(user_id, [{"from": "assistant", "message": response}])

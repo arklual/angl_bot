bf044aacb0b4bcf40add6a550fac7ce94bd8faf8
arklual <arklual1@gmail.com>
2023-08-01 19:44:43 +0300

    fix

--- main.py
+++ main.py
@@ -51,6 +51,27 @@ async def subscribe(message: types.Message):
         payload='test-invoice-payload'
     )
 
+@dp.callback_query_handler(lambda callback:  callback.data == "subscribe")
+async def subscribe_c(callback: CallbackQuery):
+    callback.answer()
+    if PAYMENT_TOKEN_TEST.split(':')[1] == "TEST":
+        await bot.send_message(callback.message.chat.id, 'Тестовый платеж')
+    await bot.send_invoice(
+        callback.message.chat.id,
+        title="Подписка на бота",
+        description='Активация подписки на бота на 1 месяц',
+        provider_token=PAYMENT_TOKEN_TEST,
+        currency='rub',
+        photo_url='https://media.istockphoto.com/id/679762242/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81%D0%BC%D0%B5%D0%BD-%D0%B8%D0%BB%D0%B8-%D1%82%D0%BE%D1%80%D0%B3%D0%BE%D0%B2%D0%B5%D1%86-%D0%BD%D0%B0-%D1%84%D0%BE%D0%BD%D0%B4%D0%BE%D0%B2%D0%BE%D0%BC-%D1%80%D1%8B%D0%BD%D0%BA%D0%B5-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D1%8E%D1%89%D0%B8%D0%B9-%D0%B7%D0%B0-%D1%81%D1%82%D0%BE%D0%BB%D0%BE%D0%BC.jpg?s=1024x1024&w=is&k=20&c=OsEncaxRjp-sbXTQUGF7XtFfSHvG03Cvu1JNl8kis7Y=',
+        photo_width=416,
+        photo_height=234,
+        photo_size=416,
+        is_flexible=False,
+        prices=[PRICE],
+        start_parameter='one-month-subscription',
+        payload='test-invoice-payload'
+    )
+
 @dp.pre_checkout_query_handler(lambda query: True)
 async def pre_checkout_query(pre_checkout_q: types.PreCheckoutQuery):
     await bot.answer_pre_checkout_query(pre_checkout_q.id, ok=True)
